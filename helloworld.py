579af1ca74565db7e367d8e2d874ecebea66870b
atman-onit <atman.bharadwaj06@gmail.com>
2025-09-10 12:09:08 +0530

    Learning the basics

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,32 @@
-print("Hello world")
+#basics
+print("Hello world") # prints hello world
+input("Enter value: ") #takes value
+
+
+a =  input("Enter value 1: ") #takes value and assigns to variable a
+b = input("Enter value 2: ") #takes value and assigns to variable b
+
+# to print a variable we:-
+print (a)
+print (b)
+
+#we can also print like
+print("Value a is " +a)
+print("Value a is " ,a)
+print("Value a is ") 
+print (a)
+
+print(a+b) # let a=3, b=3, this code will output 33 for a+b
+print("Yes, the answer is wrong as python takes input and outputs as strings\nWhen we write a+b, + is not summation rather concatenation.\nIt prints the variables as strings")
+
+#for arithmetic operations define the input's variable type
+print("This is code after input variable type is defined")
+a = int(input("Enter value 1: "))
+b = int(input("Enter value 2: "))
+print (a+b)
+
+
+
+
+
+
